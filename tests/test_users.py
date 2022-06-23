4b9f70317fb8a51c2c092f2de541cc1225e6e0ff
Franz Bandelin <bifiroll86@gmail.com>
2022-06-23 11:43:54 +0200

    added mock added fixtures

--- tests/test_users.py
+++ tests/test_users.py
@@ -13,6 +13,15 @@ import pytest
 #     'password': 'fucktypos'
 # }
 
+# example for another fixture - check connection, return, then close
+
+# yield returns value but doesn't finish function
+# @pytest.fixture
+# def db_connection():
+#     db_conn = DBConnection('mysql://localhost/class_social')
+#     yield db_conn
+#     db_conn.close()
+
 # pytest offers fixtures to replace certain parts of tests that are repeated frequently
 
 @pytest.fixture
@@ -22,24 +31,14 @@ def test_client():
 @pytest.fixture
 def valid_user_data():
     return {
-    'name': 'Franz',
-    'age': 36,
-    'email': 'email@example.cum',
-    'password': 'fucktypos'
+        'name': 'Franz',
+        'age': 36,
+        'email': 'email@example.cum',
+        'password': 'fucktypos'
     }
-
-# example for another fixture - check connection, return, then close
-# yield returns value but doesn't finish function
-@pytest.fixture
-def db_connection():
-    db_conn = DBConnection('mysql://localhost/class_social')
-    yield db_conn
-    db_conn.close()
 
 # mock simulates register_user function and returns return_value
 # so the test is tested
-
-
 def test_given_valid_user_data_the_system_must_register_the_user_and_return_200_ok(test_client, valid_user_data):
     with patch('class_social.users.user_controller') as controller_mock:
         controller_mock.register_user = Mock(return_value=True)
